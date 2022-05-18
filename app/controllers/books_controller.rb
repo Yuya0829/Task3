e9e3cb9afc3ce0c031b7863fb792e0724b3cf918
EC2 Default User <ec2-user@ip-172-31-21-248.ec2.internal>
2022-05-18 14:59:28 +0000

    app2 task 11fin & 10 create book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,25 @@
 class BooksController < ApplicationController
 
+  def new
+    @book = Book.new
+  end
+
+  def index
+    @books = Book.all
+  end
+
   def create
-    @book = Book.new(_params)
+    @book = Book.new(book_params)
     @book.user_id = current_user.id
-    if @book.save
-      redirect_to books_path
-    else
-      render :new
-    end
+    @book.save
+    redirect_to books_path
+  end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :caption)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
 
   get 'users/show'
   get 'users/edit'
-  
-  root to:'homes#top'
+
   devise_for :users
-  
-  resources :books, only: [:new, :index, :show]
+  root to:'homes#top'
+  get "homes/about" => "homes#about", as: "about"
+
+
+
+  resources :books, only: [:new, :create, :index, :show]
 
 end
