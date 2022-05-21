3906336e1fda284f53b6dfecd3c56e27536a5782
EC2 Default User <ec2-user@ip-172-31-21-248.ec2.internal>
2022-05-21 08:58:54 +0000

    app2 task 16 on the way

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,11 +19,18 @@ class BooksController < ApplicationController
     redirect_to books_path
   end
 
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+
 
   private
 
   def book_params
-    params.require(:book).permit(:title, :caption)
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
+
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
