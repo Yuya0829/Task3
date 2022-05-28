0254da0300c927b13b0c1a5d13424fc3e464af22
EC2 Default User <ec2-user@ip-172-31-21-248.ec2.internal>
2022-05-28 09:04:42 +0000

    app2 task on the way

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,11 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if@user.save
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
 
